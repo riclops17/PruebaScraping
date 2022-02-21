33a76bac2aefa0fad0f9f329ab9dcccd61255a2d
Ricardo Bauch <riclops17@gmail.com>
2022-02-20 22:10:45 -0300

    Update webScrapingYTS.py

--- webScrapingYTS.py
+++ webScrapingYTS.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import re
 baseUrl = 'https://yts.mx/'
-headers = {'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}
+
 
 movieLinks= []
 #recorrer la cantidad deseada de paginas de yts
